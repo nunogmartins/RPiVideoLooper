692f0d8e3a7e04d2db9c6c1cf51b8d788b07ac3a
Ajay <ajusharma.mait@gmail.com>
2013-12-16 14:59:36 +0530

    bad code, bad code

--- a-Jays.sh
+++ a-Jays.sh
@@ -1,11 +1,14 @@
 declare -A list
 
+FILEPATH='/home/pi/Music/*'
+
 SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
 i=0
 
-for files in /home/pi/Music/*
+for files in $FILEPATH
   do
-    list[$i]=$files       #load files into list
+    list[$i]="$files"       #load files into list
     let i+=1
     echo $files           #display loaded files
   done
@@ -19,11 +22,11 @@ while true;
     if pgrep omxplayer > /dev/null
       then echo 'playing'
     else
-      if [ i -ge max ]
-        then break            #if end of list has been reached, break
-      fi
       omxplayer "${list[$i]}"
       let i+=1
+      if [ i -ge max ]
+        then break            #if end of list has been reached, then break
+      fi
     fi
   done
 echo back!
