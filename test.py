b3e696d990d9a7a0116de0b6096991c152f48288
Muuo Wambua <muuowambua@yahoo.com>
2014-09-18 18:57:35 +0300

    removed selfies

--- test.py
+++ test.py
@@ -1,17 +1,17 @@
 import os
 from pyomxplayer import OMXPlayer
 
-self.loop1= OMXPlayer('/home/pi/videos/background.mp4', '-o local', start_playback=True)
-self.loop2 = OMXPlayer('/home/pi/videos/Nature.mp4', '-o local', start_playback=True)
-self.loop2.toggle_pause()
+loop1= OMXPlayer('/home/pi/videos/background.mp4', '-o local', start_playback=True)
+loop2 = OMXPlayer('/home/pi/videos/Nature.mp4', '-o local', start_playback=True)
+loop2.toggle_pause()
 
-position = self.loop1.position/1000000
+position = loop1.position/1000000
 if position > 12.8:
-    self.loop1.toggle_pause()
+    loop1.toggle_pause()
 
 sleep(2)
 os.system('pkill -9 -f "/usr/bin/omxplayer.bin -s /home/pi/videos/background.mp4 -o local"')
 os.system('pkill -9 -f "/bin/bash /usr/bin/omxplayer -s /home/pi/videos/background.mp4 -o local"')
 sleep(2)
-self.loop1= OMXPlayer('/home/pi/videos/background.mp4', '-o local', start_playback=True)
-self.loop1.toggle_pause()
+loop1= OMXPlayer('/home/pi/videos/background.mp4', '-o local', start_playback=True)
+loop1.toggle_pause()
