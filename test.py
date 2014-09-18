26f55bc72533b361fe9dd107fd13fdc6e0f46a6e
Muuo Wambua <muuowambua@yahoo.com>
2014-09-18 18:47:42 +0300

    did imports and what have you

--- test.py
+++ test.py
@@ -1,6 +1,8 @@
+import os
+from pyomxplayer import OMXPlayer
 
-self.loop1= OMXPlayer(‘/home/pi/loop1.mp4′, ‘-o local’, start_playback=True, do_dict=False)
-self.loop2 = OMXPlayer(‘/home/pi/loop2.mp4′, ‘-o local’, start_playback=True,do_dict=False)
+self.loop1= OMXPlayer('/home/pi/videos/background.mp4', '-o local’, start_playback=True)
+self.loop2 = OMXPlayer('/home/pi/videos/Nature.mp4', '-o local’, start_playback=True)
 self.loop2.toggle_pause()
 
 position = self.loop1.position/1000000
@@ -8,7 +10,7 @@ if position > 12.8:
     self.loop1.toggle_pause()
 
 sleep(2)
-os.system(‘pkill -9 -f "/usr/bin/omxplayer.bin -s /home/pi/loop1.mp4 -o local"’)os.system(‘pkill -9 -f "/bin/bash /usr/bin/omxplayer -s /home/pi/loop1.mp4 -o local"’)
+os.system('pkill -9 -f "/usr/bin/omxplayer.bin -s /home/pi/videos/background.mp4 -o local"’)os.system('pkill -9 -f "/bin/bash /usr/bin/omxplayer -s /home/pi/videos/background.mp4 -o local"’)
 sleep(2)
-self.loop1= OMXPlayer(‘/home/pi/loop1.mp4′, ‘-o local’, start_playback=True, do_dict=False)
+self.loop1= OMXPlayer('/home/pi/videos/background.mp4', '-o local’, start_playback=True)
 self.loop1.toggle_pause()
